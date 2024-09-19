f54bbdb1db0dee1ab2e5a532f44aa3935037f790
Jose A. Canto <jose.a.c.lopez@gmail.com>
2024-09-19 23:11:33 +0200

    add redux delete post

--- app/src/redux/postsSlice.ts
+++ app/src/redux/postsSlice.ts
@@ -2,19 +2,37 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 
 interface Post {
   id: string
+  author: string
+  title: string
+  description: string
+  image: string
+  date: string
+  liked: string[]
+  comments: {
+    author: string
+    text: string
+    date: string
+  }[]
 }
+type PostsState = Post[]
 
-const initialState: Post[] = []
+const initialState: PostsState = []
 
 export const postsSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {
     setPosts: (state, action: PayloadAction<Post[]>) => {
-      state.push(...action.payload)
+      if (action.payload.length === 0) {
+        return state
+      }
+      return action.payload
+    },
+    deletePost: (state, action: PayloadAction<string>) => {
+      return state.filter((post) => post.id !== action.payload)
     }
   }
 })
 
-export const { setPosts } = postsSlice.actions
+export const { setPosts, deletePost } = postsSlice.actions
 export default postsSlice.reducer
